0102584131f283e712d05ac8758c02346da13884
KaterinaNov <katya150997@yandex.ru>
2018-07-08 01:19:42 +0400

    Убрано все лишнее из проекта Задача #3

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <videohosting title="Видеохостинг"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import videohosting from '@/components/videohosting.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    videohosting
   }
 }
 </script>
