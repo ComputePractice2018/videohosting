7934f8b7033b0c0d66a46fb85c1e5e28262431d7
MikeOst <mihail.ostras@rambler.ru>
2018-07-08 22:18:57 +0300

    Добавлены аргументы командной строки Задание #4

--- backend/main.go
+++ backend/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"github.com/ComputePractice2018/videohosting/backend/utils"
 )
 
 func main() {
-	fmt.Println(utils.GetHelloWorldString("Michael"))
+	name := flag.String("name", "Michael", "имя для приветствия")
+	flag.Parse()
+	fmt.Println(utils.GetHelloWorldString(*name))
 }
