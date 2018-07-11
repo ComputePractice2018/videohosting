a7007b79071c4eba6dade9e65046f8d2dc98dfd6
MikeOst <mihail.ostras@rambler.ru>
2018-07-11 14:01:35 +0300

    Протестированы и отрефакторены данные Задача #19

---
+++ backend/data/videos_test.go
@@ -0,0 +1,46 @@
+package data
+
+import "testing"
+
+var testVideos = []Video{
+	{
+		Title:       "Видео 1",
+		Description: "Описание 1",
+		Link:        "Ссылка 1",
+	},
+	{
+		Title:       "Видео 2",
+		Description: "Описание 2",
+		Link:        "Ссылка 2",
+	},
+}
+
+func TestAddVideo(t *testing.T) {
+	cl := NewVideoList()
+
+	cl.AddVideo(testVideos[0])
+
+	if cl.GetVideos()[0] != testVideos[0] {
+		t.Errorf("AddVideo is not working")
+	}
+}
+
+func TestDeleteVideo(t *testing.T) {
+	cl := NewVideoList()
+	cl.AddVideo(testVideos[0])
+	cl.AddVideo(testVideos[1])
+
+	err := cl.DeleteVideo(0)
+
+	if cl.GetVideos()[0] != testVideos[1] {
+		t.Errorf("DeleteContact is not working")
+	}
+	if err != nil {
+		t.Errorf("Unexpected DeleteContact error")
+	}
+
+	err = cl.DeleteVideo(-1)
+	if err == nil {
+		t.Errorf("Nothandled out of range error")
+	}
+}
